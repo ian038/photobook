59b487a1a94f056dcf5b2977ce210f7fdc731e55
Ian Phua <ianphua3@gmail.com>
2021-08-23 12:13:30 -0400

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/tailwind.css'
+import Amplify from 'aws-amplify'
+import '@aws-amplify/ui-vue'
+import awsexports from './aws-exports'
+
+Amplify.configure(awsexports)
 
 createApp(App).use(store).use(router).mount('#app')
