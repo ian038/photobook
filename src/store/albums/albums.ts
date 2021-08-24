9ed6d35a15e3d26771126adee03c195582d71a92
Ian Phua <ianphua3@gmail.com>
2021-08-24 14:30:57 -0400

    Completed album details page

--- src/store/albums/albums.ts
+++ src/store/albums/albums.ts
@@ -1,7 +1,9 @@
-import { API, graphqlOperation } from 'aws-amplify'
-import { createAlbum as createAlbumMutation } from '@/graphql/mutations'
+import { API, graphqlOperation, Storage } from 'aws-amplify'
+import { createAlbum as createAlbumMutation, createPhoto as createPhotoMutation } from '@/graphql/mutations'
 import { getAlbum as getAlbumQuery, listAlbums as listAlbumsQuery } from '@/graphql/queries'
 import { GraphQLResult } from '@aws-amplify/api-graphql'
+import { v4 as uuidv4 } from 'uuid'
+import awsconfig from '@/aws-exports'
 
 export const albumDetails = {
   namespaced: true,
@@ -12,16 +14,17 @@ export const albumDetails = {
     }
   },
   actions: {
-    async createAlbum (_, newAlbum): Promise<void> {
+    async createAlbum ({ dispatch }, newAlbum: Record<string, string>): Promise<void> {
       try {
         await API.graphql(graphqlOperation(createAlbumMutation, { input: newAlbum }))
+        dispatch('listAlbums')
       } catch (error) {
         console.log('Create ALbum error', error)
       }
     },
 
-    async getAlbum (_, albumId): Promise<GraphQLResult<any> | any> {
-      return await API.graphql(graphqlOperation(getAlbumQuery, { input: albumId }))
+    async getAlbum (_, albumId: number): Promise<GraphQLResult<any> | any> {
+      return await API.graphql(graphqlOperation(getAlbumQuery, { id: albumId }))
     },
 
     async listAlbums ({ commit }): Promise<any> {
@@ -29,6 +32,40 @@ export const albumDetails = {
         data: any
       }
       commit('setAlbums', albumsData.data.listAlbums.items)
+    },
+
+    async createPhoto (_, data): Promise<string> {
+      const {
+        aws_user_files_s3_bucket: bucket,
+        aws_user_files_s3_bucket_region: region
+      } = awsconfig
+      const { file, type: mimeType, id } = data
+      const extension = file.name.substr(file.name.lastIndexOf('.') + 1)
+      const photoId = uuidv4()
+      const key = `images/${photoId}.${extension}`
+      const inputData = {
+        id: photoId,
+        photoAlbumId: id,
+        contentType: mimeType,
+        fullsize: {
+          key,
+          region,
+          bucket
+        }
+      }
+
+      try {
+        await Storage.put(key, file, {
+          level: 'protected',
+          contentType: mimeType,
+          metadata: { albumId: id, photoId }
+        })
+        await API.graphql(graphqlOperation(createPhotoMutation, { input: inputData }))
+        return Promise.resolve('Upload Success!')
+      } catch (error) {
+        console.log('create photo error', error)
+        return Promise.reject(error)
+      }
     }
   },
   getters: {
--- src/views/AlbumDetails.vue
+++ src/views/AlbumDetails.vue
@@ -1,13 +1,95 @@
 <template lang="">
   <div>
-    <h1>Album Details Page</h1>
+    <h3 class="text-4xl font-semibold">Album {{ albumName }}</h3>
+    <div class="flex w-full mt-10 items-center justify-center bg-grey-lighter">
+      <form enctype="multipart/form-data" novalidate>
+        <label
+          class="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue hover:text-green-600"
+        >
+          <svg
+            class="w-8 h-8"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z"
+            />
+          </svg>
+          <span class="mt-2 text-base leading-normal">Select a file</span>
+          <input @change="onFileChange" accept="image/*" type="file" class="hidden" />
+        </label>
+      </form>
+    </div>
+    <div class="text-2xl mt-4">List Of Photos</div>
+    <div class="flex flex-wrap p-10 justify-center m-auto w-full" v-if="photos">
+      <div class="shadow-xl ml-4 mt-4 w-4/12" v-for="(photo, idx) in photos" :key="idx">
+        <amplify-s3-image
+          level="protected"
+          :img-key="photo.fullsize.key"
+          class="w-4/12"
+        ></amplify-s3-image>
+        <div v-if="photo.createdAt && photo.gps">
+          <button class="btn-green mb-4">Create Album</button>
+          <!-- <ul>
+            <li>Created At {{ photo.createdAt }}</li>
+            <li>
+              latitude
+              {{ photo.gps.latitude }}
+            </li>
+            <li>longitude At {{ photo.gps.longitude }}</li>
+          </ul> -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-<script>
-export default {
 
+<script lang='ts'>
+
+type dataProps = {
+  photos: Array<Record<string, string>>
+  albumName: string
+}
+
+export default {
+  mounted (): void {
+    this.getPhotos()
+  },
+  data: (): dataProps => ({
+    photos: [],
+    albumName: ''
+  }),
+  methods: {
+    async onFileChange (file): Promise<void> {
+      if (!file.target || !file.target.files[0]) return
+      try {
+        await this.$store.dispatch('albumDetails/createPhoto', {
+          file: file.target.files[0],
+          id: this.id
+        })
+        this.getPhotos()
+      } catch (error) {
+        console.log('Create photo error', error)
+      }
+    },
+
+    async getPhotos (): Promise<void> {
+      const album = await this.$store.dispatch('albumDetails/getAlbum', this.id)
+      this.photos = album.data.getAlbum.photos.items
+      this.albumName = album.data.getAlbum.name
+    }
+  },
+  computed: {
+    id (): void {
+      return this.$route.params.id
+    }
+  }
 }
 </script>
-<style lang="">
 
+<style lang="scss" scoped>
+amplify-s3-image {
+  --width: 75%;
+}
 </style>
